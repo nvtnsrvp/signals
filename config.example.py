b4e06078a5a449b111b87f20d818cf91b2ac248b
Navi Tansaraviput <nvtnsrvp@gmail.com>
2025-02-10 18:13:07 -0500

    Add an example config file

---
+++ config.example.py
@@ -0,0 +1,13 @@
+# Description: Configuration file for the project
+
+limit = 1
+
+# Praw
+
+client_id='VIpynGlm23Ajw9geQqZTLg'
+client_secret='nhwHl2uge60cfbfFt81YbVrNHFOpSw'
+user_agent='macos:com.example.signals:v0 (by /u/nvtns)'
+
+# Tickers
+
+tickers_path = 'data/nasdaq-listed-short.csv'
