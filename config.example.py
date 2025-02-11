8fa69bada00091e90d1957c29930d36b5fb013e3
Navi Tansaraviput <nvtnsrvp@gmail.com>
2025-02-11 02:22:23 -0500

    Add an example config file and requirement.txt

--- config.example.py
+++ config.example.py
@@ -1,13 +1,19 @@
 # Description: Configuration file for the project
 
-limit = 1
+submission_limit = 10
 
 # Praw
 
-client_id='VIpynGlm23Ajw9geQqZTLg'
-client_secret='nhwHl2uge60cfbfFt81YbVrNHFOpSw'
-user_agent='macos:com.example.signals:v0 (by /u/nvtns)'
+client_id='REDDIT_CLIENT_ID'
+client_secret='REDDIT_CLIENT_SECRET'
+user_agent='PLATFORM:PROJECT_NAME:VERSION (by /u/REDDIT_USERNAME)'
 
 # Tickers
 
-tickers_path = 'data/nasdaq-listed-short.csv'
+tickers_path = 'data/nasdaq-listed.csv'
+
+# PostgreSql Database
+host = 'localhost'
+dbname = 'reddit'
+user = 'postgres'
+password = ''
